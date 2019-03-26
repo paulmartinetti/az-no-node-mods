f07679c0e30218d2a5357b5f2ed220f22f228f01
Paul Martinetti <paul@digscitech.com>
2019-03-26 13:46:45 -0400

    tiny update to test redeploy

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express')
 const app = express();
 
 app.get('/', (req, res)=>{
-    res.send('hello azzzzure no node modules :)')
+    res.send('hello azzzzure no node modules...and an update :)')
 })
 
 let port = process.env.PORT || 5000;
